a110e01928c6b973587ead82f050f213aacc6bdc
devendra-coder <devendras162002@gmial.com>
2025-07-12 16:53:56 +0530

    javascript file completed

--- project_one/index.js
+++ project_one/index.js
@@ -1,4 +1,14 @@
 const panels = document.querySelectorAll('.panel');
-panels.forEach((panel)=>{
-    panel.addEventListener('click',)
+panels.forEach(panel=>{
+    panel.addEventListener('click',()=>{
+        removeactiveclasses();
+        panel.classList.add('active');
+    })
 })
+
+function removeactiveclasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active');
+    }
+    )
+}
